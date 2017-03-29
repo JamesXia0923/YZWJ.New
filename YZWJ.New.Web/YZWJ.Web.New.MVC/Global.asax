61ed64f258ea76047bfb0143f598ed1f0c3cb73c
JamesXia0923 <james199109@sina.com>
2017-03-29 22:16:52 +0800

    Update architect

---
+++ YZWJ.New.Web/YZWJ.Web.New.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YZWJ.Web.New.MVC.MvcApplication" Language="C#" %>
---
+++ YZWJ.New.Web/YZWJ.Web.New.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YZWJ.Web.New.MVC.Startup))]
+namespace YZWJ.Web.New.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
