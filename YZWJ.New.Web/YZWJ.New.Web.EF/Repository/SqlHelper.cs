6836b396d7430fd14cac723c081722a8dc72ac79
JamesXia0923 <james199109@sina.com>
2017-03-30 22:39:55 +0800

    Add DB connection

---
+++ YZWJ.New.Web/YZWJ.New.Web.EF/Repository/SqlHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YZWJ.New.Web.EF.Repository
+{
+    public class SqlHelper
+    {
+        protected SqlConnection conn;
+        //打开连接
+        public bool OpenConnection()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            try
+            {
+                bool result = true;
+                if (conn.State.ToString() != "Open")
+                {
+                    conn.Open();
+                }
+                return result;
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+        }
+    }
+}
